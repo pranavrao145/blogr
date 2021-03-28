2826bc967843dd75e82047939e4830cc8f6617db
pranavrao145 <56097527+pranavrao145@users.noreply.github.com>
2021-03-27 22:27:38 -0400

    form styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_201955) do
+ActiveRecord::Schema.define(version: 2021_03_27_202421) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_posts_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
